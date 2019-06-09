32632496da5b29869db7420c60e980f48eb44686
erkdev <dev@eurekacoin.io>
2019-06-09 17:46:47 +0200

    update sanity check

--- src/validation.cpp
+++ src/validation.cpp
@@ -3010,7 +3010,7 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
     //only start checking this error after block 5000 and only on testnet and mainnet, not regtest
     if(pindex->nHeight > 5000 && !Params().GetConsensus().fPoSNoRetargeting) {
         //sanity check in case an exploit happens that allows new coins to be minted
-        if(pindex->nMoneySupply > (uint64_t)(149999900 + ((pindex->nHeight - 5000) * 0.00001)) * COIN){
+        if(pindex->nMoneySupply > (uint64_t)(149000000 + ((pindex->nHeight - 5000) * 0.00001)) * COIN){
             return state.DoS(100, error("ConnectBlock(): Unknown error caused actual money supply to exceed expected money supply"),
                              REJECT_INVALID, "incorrect-money-supply");
         }
